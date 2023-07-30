8e25eccec02e3be6215e4f2003acc6dc7975df22
Norralak Sukaram <78573372+norralak@users.noreply.github.com>
2023-07-29 23:54:33 -0400

    Create Table and Get All Takes

--- backend/main.py
+++ backend/main.py
@@ -1,41 +1,45 @@
-import datetime
-import math
-import random
-import time
+
+from time import sleep
 from fastapi import FastAPI, HTTPException, status
 import psycopg2
 from psycopg2.extras import RealDictCursor
-import schema
+import schemas
+#Connect to DB 
+while True:
 
-#Create Instance
+    try:
+        konnect = psycopg2.connect(database='phantom', user='postgres', password='2542', cursor_factory=RealDictCursor)
+        kursor = konnect.cursor()
+        print("Phantom Connected")
+        break
+
+    except psycopg2.Error as e:
+        print('Phantom Missed')
+        print(f'Error: {e}')
+        sleep(13)    
+    
+
 
 app = FastAPI()
 
-#PSychoPG DB
-
-while True:
+@app.get('/new', status_code=status.HTTP_200_OK)
+def makeTable():
     try:
-        konect = psycopg2.connect(host='localhost', database='phantom', user='postgres', password='2542', cursor_factory=RealDictCursor)
-        cursor = konect.cursor()
-        print("Database Connection Successful")
-        break
-    except Exception as error:
-        print("Database Connection Failed")
-        print("Error: ", error)
-        time.sleep(13)
+        kursor.execute(""" CREATE TABLE HotTakes(id SERIAL NOT NULL, username character varying NOT NULL, category character varying NOT NULL, hot_take character varying NOT NULL, created time with time zone NOT NULL DEFAULT NOW(), PRIMARY KEY (id) );""")
+    except psycopg2.Error as e:
+        raise HTTPException(status_code=status.HTTP_412_PRECONDITION_FAILED, detail=f'{e}')
+    konnect.commit()
+    return {'Message: Table Created'}
+
+@app.get('/hottake', status_code=status.HTTP_200_OK)
+def getHotTakes():
+    kursor.execute(""" SELECT * FROM HotTakes""")
+    return kursor.fetchall()
 
 
-#Homepage
-stuff = {}
-
-@app.get('/')
-def home():
-    return {'Welcome':'Whats good bitch'}
-
-@app.post('/hot/take', status_code=status.HTTP_201_CREATED)
-def createHotTake(payload: schema.HotTake):
-    cursor.execute(""" INSERT INTO HotTakes (name, hot_take, category) VALUES (%s, %s, %s) RETURNING *; """, (payload.name, payload.hot_take, payload.category))
-    hot_take = cursor.fetchone()
-    konect.commit()
+@app.post('/hottake', response_model=schemas.HotTakeResp, status_code=status.HTTP_201_CREATED)
+def createHotTake(payload: schemas.HotTake):
+    kursor.execute("""INSERT INTO HotTakes (username, category, hot_take) VALUES(%s, %s, %s) RETURNING *;""", (payload.username, payload.category, payload.hot_take))
+    konnect.commit()
+    hot_take = kursor.fetchone()
     return hot_take
-
